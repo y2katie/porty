50f3417d8181becd1281b6091b7b02a291042481
katyvonkaty <kj0203@gmail.com>
2020-11-23 11:22:10 -0500

    updated

--- src/components/OnlineLeasing.js
+++ src/components/OnlineLeasing.js
@@ -14,9 +14,9 @@ export default function OnlineLeasing() {
       link="https://apply.funnelleasing.com/2/login"
       img = "23.png"
       problem="Problem"
-      problemContent="Small business owners need a way to talk with their customers, but they don’t always want to give out their personal cell phone number. They need something professional and secure to stay in touch and appear online at all hours. "
+      problemContent="Renting an apartment in a market like New York City was always a time sensitive problem. Gatering documents, staying ontop of roommates to submit documents, submitting bank statements, inviting guarantors, etc is a long winded process. "
       solution="Solution"
-      solutionContent="Hiline gives customers a business phone number as the main feature, which users can put on their website, business cards, and social media. They can set office hours for communication, and send customers to a professional voicemail or text messaging when they’re not around.  "
+      solutionContent="Online Leasing gives renters an easier way to rent an apartment. By filling out the app online in the comfort of their home, it makes reanting an apartment almost enjoyable."
       img2 = "24.png"
 
     />
