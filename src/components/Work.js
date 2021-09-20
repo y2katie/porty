5dbff9f7815e6d0dde4ef70ebbfee87b3ada520d
katie <kj0203@gmail.com>
2021-09-20 16:52:52 -0400

    updated

--- src/components/Work.js
+++ src/components/Work.js
@@ -4,40 +4,38 @@ import CardExampleCard from "./Cards";
 import { Menu, Segment, Card } from "semantic-ui-react";
 import { Grid, Image, Container } from "semantic-ui-react";
 
-
 import { Link } from "react-router-dom";
 import bb from "../assets/images/bb.jpg";
 import roar from "../assets/images/roar.jpg";
 import star from "../assets/images/star.png";
 import yoga from "../assets/images/yoga.png";
-const src = 'https://i.pinimg.com/originals/b9/c9/76/b9c9764583f7f5bdc1de5413bbb6952e.jpg'
-
+const src =
+  "https://i.pinimg.com/originals/b9/c9/76/b9c9764583f7f5bdc1de5413bbb6952e.jpg";
 
 const Work = () => (
-  <Container fluid style={{ marginTop: "50px" }}>
+  <Container responsive fluid style={{ marginTop: "50px" }}>
     <Grid>
       <Grid.Row>
-        <Grid.Column width={3}>
+        <Grid.Column mobile={4} tablet={8} computer={4}>
           <Header />
         </Grid.Column>
 
-<Grid.Column width={12}>
-        <Card.Group itemsPerRow={4}>
-    <Card raised image={src} />
-    <Card raised image={src} />
-    <Card raised image={src} />
-    <Card raised image={src} />
-    <Card raised image={src} />
-    <Card raised image={src} />
-    <Card raised image={src} />
-    <Card raised image={src} />
-    <Card raised image={src} />
-    <Card raised image={src} />
-    <Card raised image={src} />
-    <Card raised image={src} />
-  </Card.Group>
-  </Grid.Column>
-
+        <Grid.Column style={{padding:"20px"}} mobile={16} tablet={8} computer={12}>
+          <Card.Group itemsPerRow={4}>
+            <Card raised image={src} />
+            <Card raised image={src} />
+            <Card raised image={src} />
+            <Card raised image={src} />
+            <Card raised image={src} />
+            <Card raised image={src} />
+            <Card raised image={src} />
+            <Card raised image={src} />
+            <Card raised image={src} />
+            <Card raised image={src} />
+            <Card raised image={src} />
+            <Card raised image={src} />
+          </Card.Group>
+        </Grid.Column>
       </Grid.Row>
     </Grid>
   </Container>
