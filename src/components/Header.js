8b4da3994d0cac0cf192da948d025be56d92ed49
katie <kj0203@gmail.com>
2021-09-20 18:36:12 -0400

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,23 +4,36 @@ import { Grid, Image, Menu, Segment, Input, Label } from "semantic-ui-react";
 
 const Header = () => (
   <Grid>
-      <Menu vertical>
+
+      <Menu style={{padding:"30px", border:"none !important"}} className="ui secondary vertical pointing menu fixed">
+
+      <Menu.Item>
+        <Image src="https://i.etsystatic.com/14192769/r/il/522e68/1948291857/il_570xN.1948291857_h91n.jpg" />
+      </Menu.Item>
+
+
         <Menu.Item>
-          <Label color="teal">1</Label>
-          Inbox
+          Product
+        </Menu.Item>
+        <Menu.Item>
+          Pastry
         </Menu.Item>
 
         <Menu.Item>
-          <Label>51</Label>
-          Spam
+          Watercolor
         </Menu.Item>
 
         <Menu.Item>
-          <Label>1</Label>
-          Updates
+Web        </Menu.Item>
+
+        <Menu.Item>
+          Illustration
         </Menu.Item>
         <Menu.Item>
-          <Input icon="search" placeholder="Search mail..." />
+          About
+        </Menu.Item>
+        <Menu.Item>
+          <Input icon="search" placeholder="Contact me" />
         </Menu.Item>
       </Menu>{" "}
 
--- src/components/Work.js
+++ src/components/Work.js
@@ -9,6 +9,10 @@ import bb from "../assets/images/bb.jpg";
 import roar from "../assets/images/roar.jpg";
 import star from "../assets/images/star.png";
 import yoga from "../assets/images/yoga.png";
+import baked from "../assets/images/baked.png";
+import agent from "../assets/images/agent.png";
+
+
 const src =
   "https://i.pinimg.com/originals/b9/c9/76/b9c9764583f7f5bdc1de5413bbb6952e.jpg";
 
@@ -21,17 +25,19 @@ const Work = () => (
         </Grid.Column>
 
         <Grid.Column style={{padding:"20px"}} mobile={16} tablet={8} computer={12}>
-          <Card.Group itemsPerRow={4}>
-            <Card raised image={src} />
-            <Card raised image={src} />
-            <Card raised image={src} />
-            <Card raised image={src} />
-            <Card raised image={src} />
-            <Card raised image={src} />
-            <Card raised image={src} />
-            <Card raised image={src} />
-            <Card raised image={src} />
-            <Card raised image={src} />
+          <Card.Group itemsPerRow={3}>
+            <Card raised>
+            <Image src={yoga} wrapped ui={false} />
+            </Card>
+            <Card raised image={star} />
+            <Card raised image={roar} />
+            <Card raised image={bb} />
+            <Card raised image={baked} />
+            <Card raised image={roar} />
+            <Card raised image={agent} />
+            <Card raised image={star} />
+            <Card raised image={roar} />
+            <Card raised image={bb} />
             <Card raised image={src} />
             <Card raised image={src} />
           </Card.Group>
